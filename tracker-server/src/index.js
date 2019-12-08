f3146c1c8b13011939675223aeda8cd6bdc89c76
Serdar Mustafa <serdarmustafa1@gmail.com>
2019-12-08 15:00:58 +0200

    initial commit

--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -1,7 +1,9 @@
+require("./models/user");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 require("dotenv").config();
 
@@ -10,9 +12,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
 
-const mongoUri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-8mmaj.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+// const mongoUri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0-8mmaj.gcp.mongodb.net/test?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongoose");
@@ -22,8 +28,8 @@ mongoose.connection.on("error", err => {
   console.error("error connecting to mongoose", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
---
+++ tracker-server/src/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bCrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.pre("save", function() {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bCrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next;
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+    });
+  });
+};
+
+mongoose.model("user", userSchema);
